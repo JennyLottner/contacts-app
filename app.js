1a7ae2b4f7880a3d58f89048ac28b5c543c6e48a
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-03-25 16:19:19 +0200

    Connect root cmp and app

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+const Router = ReactRouterDOM.HashRouter
+const { Provider } = ReactRedux
+
+import { store } from "./store/store.js"
 import { RootCmp } from './RootCmp.jsx'
 
 const elContainer = document.getElementById('root')
 const root = ReactDOM.createRoot(elContainer)
-root.render(<RootCmp />)
+root.render(
+    <Provider store={store}>
+        <Router>
+            <RootCmp />
+        </Router>
+    </Provider>
+)
+
