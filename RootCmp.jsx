b343c9bdeaa17a9d6016573a5214bac5eb0174eb
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-03-25 17:04:19 +0200

    Add route to ContactDetails

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -2,6 +2,7 @@ const { Route, Routes } = ReactRouterDOM
 const Router = ReactRouterDOM.HashRouter
 
 import { HomePage } from './pages/HomePage.jsx'
+import { ContactDetails } from './pages/ContactDetails.jsx'
 
 export function RootCmp() {
     return (
@@ -10,6 +11,7 @@ export function RootCmp() {
             <main >
                 <Routes>
                     <Route element={<HomePage />} path={'/'} />
+                    <Route element={<ContactDetails />} path={'/contact/:contactId'} />
                 </Routes>
             </main>
         </Router>
