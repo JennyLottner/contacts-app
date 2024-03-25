eed1688a63cf1cbddf61670b0524dce093d50729
JennyLottner <jenny.lottner@gmail.com>
2024-03-25 16:21:12 +0200

    Add services  including contact service

---
+++ services/async-storage.service.js
@@ -0,0 +1,51 @@
+import { utilService } from "./util.service.js"
+
+export const storageService = {
+    query,
+    get,
+    post,
+    put,
+    remove,
+}
+
+function query(entityType, delay = 500) {
+    var entities = JSON.parse(localStorage.getItem(entityType)) || []
+    return new Promise(resolve => setTimeout(() => resolve(entities), delay))
+}
+
+function get(entityType, entityId) {
+    return query(entityType).then(entities => {
+        const entity = entities.find(entity => entity.id === entityId)
+        if (!entity) throw new Error(`Get failed, cannot find entity with id: ${entityId} in: ${entityType}`)
+        return entity
+    })
+}
+
+function post(entityType, newEntity) {
+    newEntity = {...newEntity}
+    newEntity.id =utilService.makeId()
+    return query(entityType).then(entities => {
+        entities.push(newEntity)
+        utilService.saveToStorage(entityType, entities)
+        return newEntity
+    })
+}
+
+function put(entityType, updatedEntity) {
+    return query(entityType).then(entities => {
+        const idx = entities.findIndex(entity => entity.id === updatedEntity.id)
+        if (idx < 0) throw new Error(`Update failed, cannot find entity with id: ${entityId} in: ${entityType}`)
+        entities.splice(idx, 1, updatedEntity)
+        utilService.saveToStorage(entityType, entities)
+        return updatedEntity
+    })
+}
+
+function remove(entityType, entityId) {
+    return query(entityType).then(entities => {
+        const idx = entities.findIndex(entity => entity.id === entityId)
+        if (idx < 0) throw new Error(`Remove failed, cannot find entity with id: ${entityId} in: ${entityType}`)
+        entities.splice(idx, 1)
+        utilService.saveToStorage(entityType, entities)
+    })
+}
