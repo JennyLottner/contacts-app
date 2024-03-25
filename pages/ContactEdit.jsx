33e2fcc111784738e7302bca395135d692e8be6f
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-03-25 19:09:58 +0200

    Edit functions inside ContactEdit cmp

--- pages/ContactEdit.jsx
+++ pages/ContactEdit.jsx
@@ -1,6 +1,8 @@
-import { contactService } from "../services/contact.service.js"
+import { saveContact } from "../store/actions/contact.actions.js"
+import { setMsg } from "../store/actions/app.actions.js"
 const { useEffect, useState } = React
 const { useParams } = ReactRouterDOM
+
 export function ContactEdit() {
     const [contactToEdit, setContactToEdit] = useState(null)
     const { contactId } = useParams()
@@ -12,28 +14,29 @@ export function ContactEdit() {
 
     function setContactDetails(ev) {
         ev.preventDefault()
-        if(!contactToEdit._id) contactService.
-
+        saveContact
+            .then(savedContact => setMsg(`${savedContact.fullname} was successfully saved to our contact list!`))
+            .catch(err => console.log('err', err))
     }
 
     function handleChange(ev) {
         ev.stopPropagation()
         const { target } = ev
-        const { name:field, type, value} = target
-        if(type === number) value = +value
+        const { name: field, type, value } = target
+        if (type === number) value = +value
         setContactToEdit((prevContact) => ({ ...prevContact, [field]: value }))
     }
 
     if (!contactToEdit) return <div className="loading-div">Loading contact...</div>
     return <section className="contact-edit">
-        <h1>Edit your contact page</h1>
+        <h1>Edit your contact details</h1>
         {contactToEdit && <form className="edit-contact-form" onSubmit={setContactDetails}>
             <label htmlFor="contact-fullname">Full name: </label>
             <input id="contact-fullname" name="fullname" type="text" name="fullname" placeholder="Your full name" value={contactToEdit.fullname || ''} onChange={handleChange} />
             <label htmlFor="contact-birthday">Birth day: </label>
             <input type="date" id="contact-birthday" name="birthday" value={contactToEdit.birthday} onChange={handleChange} />
             <label htmlFor="contact-phone">Phone number (Israeli format, xxx-xxx-xxxx): </label>
-            <input type="tel" name="phone" id="contact-phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" value={contactToEdit.phone || '' } onChange={handleChange} />
+            <input type="tel" name="phone" id="contact-phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" value={contactToEdit.phone || ''} onChange={handleChange} />
             <label htmlFor="contact-address">Address: </label>
             <input type="text" name="address" id="contact-address" value={contactToEdit.address || ''} onChange={handleChange} />
             <label htmlFor="contact-img">Profile image: </label>
