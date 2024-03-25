84bbaec81372fffb70c9ba567dfb23f70a5adb4e
JennyLottner <jenny.lottner@gmail.com>
2024-03-25 17:02:20 +0200

    Add routing to ContactIndex

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -2,6 +2,7 @@ const { Route, Routes } = ReactRouterDOM
 const Router = ReactRouterDOM.HashRouter
 
 import { HomePage } from './pages/HomePage.jsx'
+import { ContactIndex } from './pages/ContactIndex.jsx'
 
 export function RootCmp() {
     return (
@@ -10,6 +11,7 @@ export function RootCmp() {
             <main >
                 <Routes>
                     <Route element={<HomePage />} path={'/'} />
+                    <Route element={<ContactIndex />} path={'/index'} />
                 </Routes>
             </main>
         </Router>
